83b5cf22e5104dbc3e8296d73273072bcc1b8cab
rawer <jiayaguang123@gmail.com>
2019-08-21 15:10:31 +0800

    隐藏 xpsoed 关键字

--- runtime/mirror/art_method-inl.h
+++ runtime/mirror/art_method-inl.h
@@ -503,7 +503,7 @@ inline const DexFile::TypeList* ArtMethod::GetParameterTypeList() {
 
 inline const char* ArtMethod::GetDeclaringClassSourceFile() {
   if (UNLIKELY(IsXposedHookedMethod())) {
-    return "<Xposed>";
+    return "<System>";
   }
   return GetInterfaceMethodIfProxy()->GetDeclaringClass()->GetSourceFile();
 }
